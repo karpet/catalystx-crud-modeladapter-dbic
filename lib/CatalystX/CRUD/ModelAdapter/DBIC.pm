24763e12057e298d0042fab2a5db10a81fc41375
Peter Karman <karpet@peknet.com>
2014-04-24 14:12:12 +0000

    add mailing list url

--- lib/CatalystX/CRUD/ModelAdapter/DBIC.pm
+++ lib/CatalystX/CRUD/ModelAdapter/DBIC.pm
@@ -572,6 +572,10 @@ You can also look for information at:
 
 =over 4
 
+=item * Mailing List
+
+L<https://groups.google.com/forum/#!forum/catalystxcrud>
+
 =item * AnnoCPAN: Annotated CPAN documentation
 
 L<http://annocpan.org/dist/CatalystX-CRUD-ModelAdapter-DBIC>
